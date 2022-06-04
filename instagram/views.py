12f0ed62c8d4eeedce5d4a8c8e0c0654ce181bb7
rossyvee <roseakinyi001@gmail.com>
2022-06-04 14:03:09 +0300

    adding the function for the profile

--- instagram/views.py
+++ instagram/views.py
@@ -18,5 +18,25 @@ def index(request):
         'users': users,
 
     }
-    return render(request, 'plata/index.html', context)
+    return render(request, 'instagram/index.html', context)
 
+
+def profile(request, username):
+    images = request.user.profile.posts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateUserProfileForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'prof_form': profile_form,
+        'images': images,
+
+    }
+    return render(request, 'instagram/profile.html', context)

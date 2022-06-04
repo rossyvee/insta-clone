92d7ce04d1b36d24d21b15031e16c633216d0890
rossyvee <roseakinyi001@gmail.com>
2022-06-04 15:21:31 +0300

    imporing models from django database

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+#     profile_picture = models.ImageField(upload_to='images/', default='default.png')
+#     bio = models.TextField(max_length=500, default="My Bio", blank=True)
+#     name = models.CharField(blank=True, max_length=120)
+#     location = models.CharField(max_length=60, blank=True)
+
+

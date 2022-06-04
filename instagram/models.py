25af35de7a0d06ea005d70932836f72db3c46677
rossyvee <roseakinyi001@gmail.com>
2022-06-04 15:25:26 +0300

    creating the profile class and the table model

--- instagram/models.py
+++ instagram/models.py
@@ -4,13 +4,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-#     profile_picture = models.ImageField(upload_to='images/', default='default.png')
-#     bio = models.TextField(max_length=500, default="My Bio", blank=True)
-#     name = models.CharField(blank=True, max_length=120)
-#     location = models.CharField(max_length=60, blank=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
 
 

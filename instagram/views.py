fae0109b143cf3bb2fad98487fc7be147a121451
rossyvee <roseakinyi001@gmail.com>
2022-06-04 12:47:51 +0300

    creating a function for index.html

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+def index(request):
+    images = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = PostForm()
+    context = {
+        'images': images,
+        'form': form,
+        'users': users,
+
+    }
+    return render(request, 'plata/index.html', context)
 

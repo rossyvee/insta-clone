b281c22ba3be95f0630f49f4a2c5e93ce4400a16
rossyvee <roseakinyi001@gmail.com>
2022-06-06 19:13:50 +0300

    adding the urls paths to the app model

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path, include
-from instagram import views
-
+from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path('',views.index, name='home'),
+    path('signup/', views.signup, name='signup'),
+   
     path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+     path('like', views.like_post, name='like_post'),
+    path('search/', views.search_profile, name='search'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow'),
+    path('post/<id>', views.post_comment, name='comment'),
+    path('post/<id>/like', views.PostLikeToggle.as_view(), name='liked'),
+    path('api/post/<id>/like', views.PostLikeAPIToggle.as_view(), name='liked-api'),
+    path('signout/',views.signout,name='signout')
 ]

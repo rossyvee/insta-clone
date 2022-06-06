0a775f223141c83f8e0d52a42255e08b7d81d01f
rossyvee <roseakinyi001@gmail.com>
2022-06-06 18:49:51 +0300

    restructruing the models module

--- instagram/models.py
+++ instagram/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    profile_picture =CloudinaryField('image')
     bio = models.TextField(max_length=500, default="My Bio", blank=True)
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
@@ -33,9 +36,9 @@ class Profile(models.Model):
         return cls.objects.filter(user__username__icontains=name).all()
 
 class Post(models.Model):
-    image = models.ImageField(upload_to='posts/')
+    image = CloudinaryField('image')
     name = models.CharField(max_length=250, blank=True)
-    caption = models.CharField(max_length=250, blank=True)
+    caption = models.TextField( blank=True)
     likes = models.ManyToManyField(User, related_name='likes', blank=True, )
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
     created = models.DateTimeField(auto_now_add=True, null=True)
@@ -75,12 +78,15 @@ class Comment(models.Model):
     class Meta:
         ordering = ["-pk"]
 
-    class Follow(models.Model):
-        follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
     followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
 
     def __str__(self):
         return f'{self.follower} Follow'
+
+
 
 
 

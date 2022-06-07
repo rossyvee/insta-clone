53bbf6da64927ff667d9beb11a5556ee0f69ca15
rossyvee <roseakinyi001@gmail.com>
2022-06-07 14:23:00 +0300

    adding and updating the app

--- social/settings.py
+++ social/settings.py
@@ -87,9 +87,11 @@ WSGI_APPLICATION = 'social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE':  'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'rose',
+    'PASSWORD':'pass123',
+    }  
 }
 
 
--- social/urls.py
+++ social/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('plata.urls')),
+    path('',include('instagram.urls')),
      path('account/', include('django.contrib.auth.urls')),
 ]
